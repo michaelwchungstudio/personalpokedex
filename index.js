7205bea5ccf9867627184d6f0c14c821a1b28d32
Michael W Chung <dryhslaw@gmail.com>
2018-10-08 20:46:01 -0400

    added class to indexjs

--- index.js
+++ index.js
@@ -1,77 +1,32 @@
-var image1 = document.getElementById('image1');
-var trainer1 = [1, 2, 3]
-var hello = [];
+var pokemonSprite = document.getElementById('currentpokesprite');
+var pokeID = document.getElementById('pokeid');
+var pokeName = document.getElementById('pokename');
+var pokeHP = document.getElementById('pokehp');
+var pokeAttack = document.getElementById('pokeattack');
+var pokeDefense = document.getElementById('pokedef');
+var pokeAbilities = document.getElementById('pokeabilities');
+var vapButton = document.getElementById('vapbutton');
+var joltButton = document.getElementById('joltbutton');
+var flareButton = document.getElementById('flarebutton');
 
-let pokeVaporeon = {};
+let pokeVaporeon;
+let pokeJolteon;
+let pokeFlareon;
 
-class Pokemon {
-  constructor(pokeObject) {
-    this.pokemonObject = pokeObject
-    this.pokemonID = pokeObject.id
-    this.healthPoints = pokeObject.stats[5].base_stat;
-    this.attackPoints = pokeObject.stats[4].base_stat;
-    this.defensePoints = pokeObject.stats[3].base_stat;
-    // this.pokemonSprite;
-    // this.pokemonAbilities;
-  }
+var listOfPokemon = [];
+var pokeCount = 0;
 
-  // createPokemon(idnum) {
-  //   var that = this;
-  //
-  //   $.ajax({url:"https://fizal.me/pokeapi/api/" + idnum + ".json",
-  //     type: "GET",
-  //     success: function(response){
-  //       that.pokemonObject = response;
-  //       that.healthPoints = response.stats[5].base_stat;
-  //       that.attackPoints = response.stats[4].base_stat;
-  //       that.defensePoints = response.stats[3].base_stat;
-  //       that.pokemonSprite = response.sprites.front_default;
-  //       that.pokemonAbilities = [];
-  //
-  //       for(let i = 0; i < response.abilities.length; i++) {
-  //         that.pokemonAbilities.push(response.abilities[i].ability.name)
-  //       }
-  //     }
-  //   })
-  // }
-}
-
-
-$.ajax({url:"https://fizal.me/pokeapi/api/" + 134 + ".json",
-  // async: false,
-  success: function(response){
-    pokeVaporeon = new Pokemon(response);
-    hello.push(pokeVaporeon);
-  }
-})
-
-console.log(hello);
-
-
-
-//
-// $.ajax({url:"https://fizal.me/pokeapi/api/" + 135 + ".json",
-//   success: function(response){
-//     pokeJolteon = new Pokemon(response);
-//     hello.push(pokeJolteon);
-//   }
-// })
-//
-// $.ajax({url:"https://fizal.me/pokeapi/api/136.json",
-//   success: function(response){
-//     pokeFlareon = new Pokemon(response);
-//     hello.push(pokeFlareon);
-//   }
-// })
-
-// TRAINER CLASS
 class Trainer {
   constructor(arrayOfPokemon) {
     this.trainerPokemon = arrayOfPokemon;
   }
 
-  addPokemon(pokeObj) {
-    this.trainerPokemon.push(pokeObj);
+  getPokemon(searchName) {
+    for(let i = 0; i < this.trainerPokemon.length; i++) {
+      if(this.trainerPokemon[i].pokemonName == searchName) {
+        return this.trainerPokemon[i];
+      }
+    }
   }
 
   all() {
@@ -79,18 +34,88 @@ class Trainer {
   }
 }
 
-// POKEMON CLASS
+class Pokemon {
+  constructor(pokeObject) {
+    this.pokemonName = pokeObject.name;
+    this.pokemonID = pokeObject.id;
+    this.healthPoints = pokeObject.stats[5].base_stat;
+    this.attackPoints = pokeObject.stats[4].base_stat;
+    this.defensePoints = pokeObject.stats[3].base_stat;
+    this.pokemonSprite = pokeObject.sprites.front_default;
 
-// var pokeVaporeon = new Pokemon(134);
-// var pokeJolteon = new Pokemon(135);
-// var pokeFlareon = new Pokemon(136);
-//
-// // ?
-// console.log(pokeVaporeon);
-// console.log(pokeVaporeon.pokemonAbilities);
-//
-// var trainerMike = new Trainer([]);
-// trainerMike.addPokemon(pokeVaporeon);
-// trainerMike.addPokemon(pokeJolteon);
-// trainerMike.addPokemon(pokeFlareon);
-// console.log(trainerMike);
+    var abil = "";
+
+    for(let i = 0; i < pokeObject.abilities.length; i++) {
+      if(i == pokeObject.abilities.length-1) {
+        abil += (pokeObject.abilities[i].ability.name.charAt(0).toUpperCase() + pokeObject.abilities[i].ability.name.slice(1) + ".")
+      }
+      else {
+        abil += (pokeObject.abilities[i].ability.name.charAt(0).toUpperCase() + pokeObject.abilities[i].ability.name.slice(1) + ", ");
+      }
+    }
+
+    this.pokemonAbilities = abil;
+  }
+}
+
+function createVaporeon() {
+  $.ajax({url:"https://fizal.me/pokeapi/api/134.json",
+      async: false,
+      type: "GET",
+      success: function(response){
+        pokeVaporeon = new Pokemon(response);
+        listOfPokemon.push(pokeVaporeon);
+    }})
+}
+
+function createJolteon() {
+  $.ajax({url:"https://fizal.me/pokeapi/api/135.json",
+      async: false,
+      type: "GET",
+      success: function(response){
+        pokeJolteon = new Pokemon(response);
+        listOfPokemon.push(pokeJolteon);
+    }})
+}
+
+function createFlareon() {
+  $.ajax({url:"https://fizal.me/pokeapi/api/136.json",
+      async: false,
+      type: "GET",
+      success: function(response){
+        pokeFlareon = new Pokemon(response);
+        listOfPokemon.push(pokeFlareon);
+    }})
+}
+
+function displayPokemon(pokeObject) {
+  pokemonSprite.style.backgroundImage = "url(" + pokeObject.pokemonSprite + ")";
+  pokeID.innerText = "ID: " + pokeObject.pokemonID;
+  pokeName.innerText = "Name: " + pokeObject.pokemonName.charAt(0).toUpperCase() + pokeObject.pokemonName.slice(1);
+  pokeHP.innerText = "HP: " + pokeObject.healthPoints;
+  pokeAttack.innerText = "Attack: " + pokeObject.attackPoints;
+  pokeDefense.innerText = "Defense: " + pokeObject.defensePoints;
+  pokeAbilities.innerText = "Abilities: " + pokeObject.pokemonAbilities;
+}
+
+createVaporeon();
+createJolteon();
+createFlareon();
+
+let mikeTrainer = new Trainer(listOfPokemon);
+
+window.addEventListener('load', function() {
+  displayPokemon(pokeVaporeon);
+});
+
+vapButton.addEventListener('click', function() {
+  displayPokemon(pokeVaporeon);
+})
+
+joltButton.addEventListener('click', function() {
+  displayPokemon(pokeJolteon);
+})
+
+flareButton.addEventListener('click', function() {
+  displayPokemon(pokeFlareon);
+})
--- index2.js
+++ index2.js
@@ -57,33 +57,3 @@ joltButton.addEventListener('click', function() {
 flareButton.addEventListener('click', function() {
   createPokemon(136);
 })
-
-// class Pokemon{
-//     constructor(ajaxNumber){
-//         this.name;
-//         this.hp;
-//         this.attack;
-//         this.defense;
-//         this.abilities = [];
-//         this.getPokemon(ajaxNumber);
-//     }
-//     getPokemon(ajaxNumber){
-//         var that = this
-//         $.ajax({url: "https://fizal.me/pokeapi/api/"+ajaxNumber+".json",
-//         success: function(result){
-//                     that.hp = result.stats[5].base_stat
-//                     that.attack = result.stats[4].base_stat
-//                     that.defense = result.stats[3].base_stat
-//                     that.abilities = [];
-//
-//                     for(let i = 0 ; i< result.abilities.length;i++){
-//                             that.abilities.push(result.abilities[i].ability.name)
-//                         }
-//                 }
-//         })
-//     }
-// }
-//
-// var pokeVaporeon = new Pokemon(134);
-// console.log(pokeVaporeon);
-// console.log(pokeVaporeon.attack);
